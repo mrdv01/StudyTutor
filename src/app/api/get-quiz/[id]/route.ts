409524ded8f07405a744a453fa776bd72555c977
Deepak verma <dv87178@gmail.com>
2025-10-14 13:48:05 +0530

    fix params type

--- src/app/api/get-quiz/[id]/route.ts
+++ src/app/api/get-quiz/[id]/route.ts
@@ -3,7 +3,7 @@ import { createClient } from "@/lib/supabase/server";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     const supabase = await createClient();
