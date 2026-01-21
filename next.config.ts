806d77a3668e2120424bf4a2f2c7f4544f7594a5
Deepak verma <dv87178@gmail.com>
2026-01-21 22:59:32 +0530

    Revert "fix pdf-parse issue"
    This reverts commit 9a197e4aed7512e22b46aca5bb3ab5fc020cb56a.

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 <<<<<<< HEAD
+<<<<<<< HEAD
   serverExternalPackages: ["pdf-parse", "pdfjs-dist", "@napi-rs/canvas"],
 =======
   /* config options here */
 >>>>>>> parent of e13b6d5 (fix type errors and pdf-parse error)
+=======
+  serverExternalPackages: ["pdf-parse", "pdfjs-dist"],
+>>>>>>> parent of 9a197e4 (fix pdf-parse issue)
 };
 
 export default nextConfig;
